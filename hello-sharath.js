9b25c0500d1156a8a50660b2b6c0fd2809c892ef
sharathdt <sharath.dt@gmail.com>
2022-04-04 11:19:55 +0530

    added my files

---
+++ hello-sharath.js
@@ -0,0 +1,20 @@
+const Ninjas = (props) => {
+  const { ninjas } = props;
+  return (
+    <div>
+      <Title title="Ninjas | Listing" />
+      <div>
+        <h1>All ninjas</h1>
+        {ninjas.map((ninja) => {
+          return (
+            <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
+              <a className={styles.single}>
+                <h3>{ninja.name}</h3>
+              </a>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
